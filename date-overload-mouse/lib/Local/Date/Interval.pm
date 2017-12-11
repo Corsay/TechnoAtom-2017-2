9f5823e5a94bb3044404dd64fe1261d05fef52c0
Dmitriy Tcibisov <blacodim@gmail.com>
2017-12-11 12:37:31 +0300

    overload operations + - ++ -- += -= < <= > >= != == <=> for Local::Date::Interval class

--- date-overload-mouse/lib/Local/Date/Interval.pm
+++ date-overload-mouse/lib/Local/Date/Interval.pm
@@ -45,7 +45,23 @@ sub _get_duration_comp {
 use overload
 	# контексты
 	'""' => '_string_con',	# строковый
-	'0+' => '_digit_con',	# числовой
+	# сложение/вычитание
+	'+' => '_add',	# числовой контекст (в случае добавления числа)
+	'-' => '_subtract',
+	# Сложение/вычитание с присваиванием
+	'+=' => '_add_assign',
+	'-=' => '_sub_assign',
+	# операции инкремента/декремента
+	'++' => '_inc',
+	'--' => '_dec',
+	# операции сравнения
+	'<' => '_sort_digit',
+	'<=' => '_sort_digit',
+	'>' => '_sort_digit',
+	'>=' => '_sort_digit',
+	'==' => '_sort_digit',
+	'!=' => '_sort_digit',
+	'<=>' => '_sort_digit',
 	fallback => 1;
 
 =head2
@@ -57,9 +73,126 @@ sub _string_con {
 	return $self->days() . " days, " . $self->hours() . " hours, " . $self->minutes() . " minutes, " . $self->seconds() . " seconds";
 }
 
-sub _digit_con {
-	my ($self) = @_;
-	return $self->duration();
+=head2
+	Числовой контекст.
+	* Интервал должен преобразовываться в число равное длительности интервала в секундах.
+	Сложение.
+	* Операция должна прибавлять указанное количество секунд к объекту.
+	* Если вторым операндом является объект типа `Local::Date::Interval`, то операция должна возвращать объект типа `Local::Date::Interval`.
+	* Если вторым операндом является число, то операция должна возвращать число (*unix timestamp*).
+	* В остальных случая должно вызываться исключение.
+=cut
+sub _add {
+	my ($date1, $date2) = @_;
+
+	if ($date2->isa('Local::Date::Interval')) {	# `Local::Date::Interval`
+		my $date = Local::Date::Interval->new(duration => $date1->duration() + $date2->duration() );
+		return $date;
+	}
+	elsif ($date2 =~ /^\d+$/) {	# Целое число
+		return $date1->duration() + $date2;
+	}
+
+	return undef;
+}
+
+=head2
+	Вычитание.
+	* Операция должна вычитать указанное количество секунд из объекта.
+	* Если вторым операндом является объект типа `Local::Date::Interval`, то операция должна возвращать объект типа `Local::Date::Interval`.
+	* Если вторым операндом является число, то операция должна возвращать число (*unix timestamp*).
+	* В остальных случая должно вызываться исключение.
+=cut
+sub _subtract {
+	my ($date1, $date2, $inverse) = @_;
+
+	# not `Local::Date::Interval` - `Local::Date::Interval` -> error
+	if ($inverse) { return undef unless $date2->isa('Local::Date::Interval'); }
+
+	if ($date2->isa('Local::Date::Interval')) {	# `Local::Date::Interval`
+		my $date = Local::Date::Interval->new(duration => $date1->duration() - $date2->duration() );
+		return $date;
+	}
+	elsif ($date2 =~ /^\d+$/) {	# Целое число
+		return $date1->duration() - $date2;
+	}
+
+	return undef;
+}
+
+=head2
+	Сложение/вычитание с присваиванием.
+	* Операции должны прибавлять/вычитать указанное количество секунд к/из объекта.<br>
+	* Если аргументом является число или объект типа `Local::Date::Interval`, то должен возращаться исходный объект типа `Local::Date::Interval`.<br>
+	* Новых объектов создаваться не должно!<br>
+	* В остальных случая должно вызываться исключение.
+=cut
+sub _add_assign {
+	my ($date1, $date2) = @_;
+
+	if ($date2->isa('Local::Date::Interval')) {	# `Local::Date::Interval`
+		$date1->duration( $date1->duration() + $date2->duration() );
+		return $date1;
+	}
+	elsif ($date2 =~ /^\d+$/) {	# Целое число
+		$date1->duration( $date1->duration() + $date2 );
+		return $date1;
+	}
+
+	die "Incorrect object\n";
+}
+
+sub _sub_assign {
+	my ($date1, $date2) = @_;
+
+	if ($date2->isa('Local::Date::Interval')) {	# `Local::Date::Interval`
+		$date1->duration( $date1->duration() - $date2->duration() );
+		return $date1;
+	}
+	elsif ($date2 =~ /^\d+$/) {	# Целое число
+		$date1->duration( $date1->duration() - $date2 );
+		return $date1;
+	}
+
+	die "Incorrect object\n";
+}
+
+=head2
+	Операции инкремента/декремента.
+	* К исходному объекту должна быть добавлена/вычтена одна секунда.
+	* Новых объектов создаваться не должно!
+=cut
+sub _inc {
+	my ($date1) = @_;
+	$date1->duration( $date1->duration() +1 );
+	return $date1;
+}
+sub _dec {
+	my ($date1) = @_;
+	$date1->duration( $date1->duration() - 1 );
+	return $date1;
+}
+
+=head2
+	Операции сравнения.
+	* Должна быть возможность сравнивать объекты между собой, а так же с временем заданным как количество секунд (*unix timestamp*).
+	* Так же объекты должны корректно сортироваться функцией *sort*.
+=cut
+sub _get_seconds {
+	my ($date1, $date2) = @_;
+
+	my $sec1 = $date1->duration();
+	my $sec2;
+	if ($date2->isa('Local::Date::Interval')) { $sec2 = $date2->duration(); }	# `Local::Date::Interval`
+	elsif ($date2 =~ /^\d+$/) { $sec2 = $date2; }	# число
+
+	return ($sec1, $sec2);
+}
+sub _sort_digit {	# сортировка как чисел (<=>)
+	my ($date1, $date2, $inverted) = @_;
+	my ($sec1, $sec2) = _get_seconds($date1, $date2);
+	return -$inverted || 1 unless ($sec2);	# если подали что-то не то то делаем как сортировка
+	return $sec1 <=> $sec2;
 }
 
 no Mouse;
